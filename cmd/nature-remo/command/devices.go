9d20e28f6f9d459befe329306f353c4caf009d51
70_10 <7010.dev@gmail.com>
2018-08-07 13:22:51 +0900

    [mod] output event time for CLI

--- cmd/nature-remo/command/devices.go
+++ cmd/nature-remo/command/devices.go
@@ -2,6 +2,7 @@ package command
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/70-10/nature-remo-go"
 )
@@ -22,14 +23,16 @@ func (c *DevicesCommand) Run(args []string) int {
 		if i != 0 {
 			c.Ui.Output("--------------------------------------------------------")
 		}
-
-		c.Ui.Output(fmt.Sprint("ID               : ", d.ID))
-		c.Ui.Output(fmt.Sprint("Name             : ", d.Name))
-		c.Ui.Output(fmt.Sprint("Temperature      : ", d.NewestEvents.Temperature.Value))
-		c.Ui.Output(fmt.Sprint("Humidity         : ", d.NewestEvents.Humidity.Value))
-		c.Ui.Output(fmt.Sprint("Illumination     : ", d.NewestEvents.Illumination.Value))
-		c.Ui.Output(fmt.Sprint("Firmware Version : ", d.FirmwareVersion))
-		c.Ui.Output(fmt.Sprint("Created At       : ", d.CreatedAt.Format("2006-01-02 15:04:05")))
+		c.Ui.Output(fmt.Sprint("ID                : ", d.ID))
+		c.Ui.Output(fmt.Sprint("Name              : ", d.Name))
+		c.Ui.Output(fmt.Sprint("Temperature       : ", d.NewestEvents.Temperature.Value))
+		c.Ui.Output(fmt.Sprint("Temperature Time  : ", format(d.NewestEvents.Temperature.CreatedAt)))
+		c.Ui.Output(fmt.Sprint("Humidity          : ", d.NewestEvents.Humidity.Value))
+		c.Ui.Output(fmt.Sprint("Humidity Time     : ", format(d.NewestEvents.Humidity.CreatedAt)))
+		c.Ui.Output(fmt.Sprint("Illumination      : ", d.NewestEvents.Illumination.Value))
+		c.Ui.Output(fmt.Sprint("Illumination Time : ", format(d.NewestEvents.Illumination.CreatedAt)))
+		c.Ui.Output(fmt.Sprint("Firmware Version  : ", d.FirmwareVersion))
+		c.Ui.Output(fmt.Sprint("Created At        : ", d.CreatedAt.In(time.Local).Format("2006-01-02 15:04:05")))
 	}
 
 	return ExitCodeOK
@@ -41,4 +44,8 @@ func (c *DevicesCommand) Synopsis() string {
 
 func (c *DevicesCommand) Help() string {
 	return ""
+}
+
+func format(t time.Time) string {
+	return t.In(time.Local).Format("2006-01-02 15:04:05")
 }
